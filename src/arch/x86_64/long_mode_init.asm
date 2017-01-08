5b3050292a61fd9e38df0443083628f590ba734d
George Slavin <george.r.slavin@gmail.com>
2017-01-07 19:41:57 -0500

    Fixed CPU exception by enabling SSE.
    Rust attempts to use SSE instructions by default but SSE wasn't enabled.
    Added routines to enable SSE to the boot.asm.  Also, the messaged
    displayed on kernel exit was changed to "OS returned!"

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -7,9 +7,12 @@ long_mode_start:
     extern rust_main
     call rust_main
 
-    ; Print `OKAYLONG` to screen
-    mov rax, 0x2f472f4e2f4f2f4c
-    mov qword [0xb8000], rax
-    mov rax, 0x2f452f442f4f2f4d
-    mov qword [0xb8008], rax
+.os_returned:
+    ; rust main returned, print `OS returned!`
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
     hlt
