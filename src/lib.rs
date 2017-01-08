7b6dd519db648555738bcc660f8b22fc47a29da9
George Slavin <george.r.slavin@gmail.com>
2017-01-07 19:34:26 -0500

    Changed to behavior of panic to simply abort the program
    The kernel doesn't support unwinding so panics should be
    a simple abort.  To fully support panic=abort, I need to compile
    libcore with the option panic=abort, but for now I've overidden
    _Unwind_Resume to avoid the errors caused by the current
    libcore having references to _Unwind_Resume.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ extern crate rlibc;
 pub extern fn rust_main() {
     let x = ["Hello", "World", "!"];
     let y = x;
+	let test = (0..3).flat_map(|x| 0..x).zip(0..);
 }
 
 /* Used for stacking unwinding.
@@ -24,3 +25,14 @@ extern fn eh_personality() {}
 #[lang = "panic_fmt"]
 #[no_mangle] 
 pub extern fn panic_fmt() -> ! {loop{}}
+
+
+/* TODO: recompile libcore with panic=abort
+ * instead of using overriding function
+ */
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+    loop {}
+}
+
